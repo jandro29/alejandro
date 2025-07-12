d28efe971ba6ba87fd86536d2bd54fc99f559803
jandro29 <95581749+jandro29@users.noreply.github.com>
2025-07-12 03:12:48 -0500

    Update formulario-hubspot.tsx

--- app/formulario-hubspot.tsx
+++ app/formulario-hubspot.tsx
@@ -5,12 +5,11 @@ import HubspotForm from '../formulario'
 import { Card } from "@/components/ui/card"
 
 export default function RegistrationForm() {
-
   return (
-<div className="flex justify-center lg:justify-start">
-  <Card className="w-[100%] bg-white border-none shadow-2xl text-white">
-    <HubspotForm />
-  </Card>
-</div>
+    <div className="flex justify-center lg:justify-start">
+      <Card className="w-[100%] bg-white border-none shadow-2xl text-white">
+        <HubspotForm />
+      </Card>
+    </div>
   )
 }
