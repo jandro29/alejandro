48698f917d2bdf79fbab1d3323499ed0732c82d4
Jose Carlos Colan <ascolanr@gmail.com>
2025-07-12 02:52:58 -0500

    fix: uso de variables en RegistrationForm

--- app/formulario-hubspot.tsx
+++ app/formulario-hubspot.tsx
@@ -10,12 +10,9 @@ export default function RegistrationForm() {
   return (
     <div className="flex justify-center lg:justify-start">
       <Card className="w-full bg-white border-none shadow-2xl">
-        {/* Contenedor que bloquea el formulario si no se aceptan los términos */}
         <div className={acceptTerms ? "opacity-100" : "opacity-50 pointer-events-none"}>
           <HubspotForm />
         </div>
-
-        {/* Checkbox de términos y condiciones */}
         <div className="mt-4 flex items-center gap-2 px-4 pb-4">
           <input
             id="terms"
